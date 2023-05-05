4e05826eb5b69f6595bf48b6a73874763fa83c2b
MarcusKyung <Kyungmj@gmail.com>
2023-05-05 09:10:33 -0700

    add updates to initial project files

---
+++ PierresBakery/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using PierresBakery.Models;
+
+namespace PierresBakery
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("Welcome to the Pierres Bakery!");
+      Console.WriteLine("How many breads would you like? A single loaf costs $5, and every third loaf is free. ");
+      Console.WriteLine("Please enter a number:");
+      // string stringNumber1 = Console.ReadLine();  
+      Console.WriteLine("How many pasteries would you like? A single pastery costs $2, and every fourth pastery is free. ");
+      Console.WriteLine("Please enter a number:");
+      // string stringNumber1 = Console.ReadLine();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+## How To Run This Project
+
+1. Clone this repo.
+2. Open your shell (e.g., Terminal or GitBash) and navigate to this project's production directory called "PierresBakery". 
+3. Run `dotnet run` in the command line to run the console app. Since this is a console application, you'll interact with it through text commands in your terminal. Take note that the user interface will vary from branch to branch.
+4. Optionally, you can run `dotnet build` to compile this console app without running it.
