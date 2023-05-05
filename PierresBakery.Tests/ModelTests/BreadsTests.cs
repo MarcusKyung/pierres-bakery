f9697365251ff4126b233414adfa8ea465f21ebc
MarcusKyung <Kyungmj@gmail.com>
2023-05-05 09:11:03 -0700

    pass bread constructor test

---
+++ PierresBakery.Tests/ModelTests/BreadsTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PierresBakery.Models;
+using System;
+
+namespace PierresBakery.Tests
+{
+  [TestClass]
+  public class BreadsTests
+  {
+    [TestMethod]
+    public void BreadsConstructor_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread();
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
--- PierresBakery/Models/Breads.cs
+++ PierresBakery/Models/Breads.cs
@@ -2,6 +2,9 @@ namespace PierresBakery.Models
 {
   public class Bread
   {
-    // Code here.
+    public Bread()
+    {
+      
+    }
   }
 }
