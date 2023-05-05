e664deb02f2f1f2f2e55056aaeeba3449af017eb
MarcusKyung <Kyungmj@gmail.com>
2023-05-05 09:57:19 -0700

    pass test for get loafs

--- PierresBakery.Tests/ModelTests/BreadsTests.cs
+++ PierresBakery.Tests/ModelTests/BreadsTests.cs
@@ -10,8 +10,20 @@ namespace PierresBakery.Tests
     [TestMethod]
     public void BreadsConstructor_CreatesInstanceOfBread_Bread()
     {
-      Bread newBread = new Bread();
+      Bread newBread = new Bread(3);
       Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+
+    [TestMethod]
+    public void GetBreadOrder_ReturnsBreadOrder_Int()
+    {
+      //Arrange
+      int quantity = 3;
+      Bread newBread = new Bread(quantity);
+      //Act
+      int result = newBread.Loafs;
+      //Assert
+      Assert.AreEqual(quantity, result);
     }
   }
 }
